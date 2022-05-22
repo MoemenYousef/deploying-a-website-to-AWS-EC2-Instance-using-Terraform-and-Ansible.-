9252441a43cd9b2d1eb87dfe0ca0c522158df6b0
MoemenYousef <46429290+MoemenYousef@users.noreply.github.com>
2022-05-22 23:25:38 +0200

    Add files via upload

---
+++ create_ec2_instance.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+  access_key = "AKIAQNFP3FUJBR4DBTO9"
+  secret_key = "XPaChv/LSOaUv/B5bb51MPdJGcmNPkGeRQkHTY5d"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allw http-https-ssh"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_key_pair" "my_blog_key" {
+  key_name = "my_tf_key"
+  public_key = file("~/.ssh/myblog-key.pub")
+}
+
+
+
+resource "aws_instance" "ec2_instance_from_tf" {
+  ami = "ami-0c4f7023847b90238"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.my_blog_key.key_name
+  security_groups = ["allw http-https-ssh"]
+
+  tags = {
+    Name = "ec2_instance_from_tf"
+  }
+}
+
